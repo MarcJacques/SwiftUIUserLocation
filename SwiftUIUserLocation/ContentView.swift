69e3dcda9eec499867fcf7853b932cf6ac63c3cd
Marc <marcjacques@me.com>
2022-01-30 19:54:01 -0500

    Initial Commit

---
+++ SwiftUIUserLocation/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  SwiftUIUserLocation
+//
+//  Created by Marc Jacques on 1/30/22.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ SwiftUIUserLocation/SwiftUIUserLocationApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUIUserLocationApp.swift
+//  SwiftUIUserLocation
+//
+//  Created by Marc Jacques on 1/30/22.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUIUserLocationApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
